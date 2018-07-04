fbe88d711022578134d8699daf36ffddda9e1083
adarshkr <kr.adarsh002@gmail.com>
2018-07-04 22:30:16 +0530

    Rock Paper Scissors Passing Test Cases

--- unit_integration/index.js
+++ unit_integration/index.js
@@ -1,3 +1,22 @@
 module.exports = {
+  rockPaperScissors: function (a, b) {
+    // Both players playing same thing
+    if (a === b) {
+      return 'tie';
+    }
 
+    // Conditions where 'a' wins
+    if ((a === 'rock' && b === 'scissors') ||
+        (a === 'paper' && b === 'rock') ||
+        (a === 'scissors' && b === 'paper')) {
+        return 'a';
+    }
+
+    // Conditions where 'b' wins
+    if ((a === 'rock' && b === 'paper') ||
+        (a === 'paper' && b === 'scissors') ||
+        (a === 'scissors' && b === 'rock')) {
+        return 'b';
+    }
+  }
 }
